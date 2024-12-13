fbbe8998b2b5a242c840c1f0e92eb33ecb223f29
Ortem12 <osipoff4rtiom@yandex.ru>
2024-12-13 13:31:31 +0300

    Added type()

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("hello lab!")
+
+if __name__ == '__main__':
+    print("Hello lab_med_inf!")
+    
+    print(type(1))  # <class 'int'>
+    print(type(1.0))  # <class 'float'>
+    print(type("Hello"))  # <class 'str'>
+    print(type(True))  # <class 'bool'>
+    print(type([1, 2, 3]))  # <class 'list'>
+    print(type((1, 2, 3)))  # <class 'tuple'>
+    print(type({'key': 'value'}))  # <class 'dict'>
